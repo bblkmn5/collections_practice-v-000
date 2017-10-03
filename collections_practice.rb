1ff3fc7f231a753da3bae33f84db6fca010ae0f7
Ben Blackman <bblkmn5@gmail.com>
2017-10-03 03:48:08 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,3 +17,9 @@ def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end
+#def swap_elements_from_to(array, index, destination_index)
+  
+def reverse_array(array)
+  arrays = array.sort {|a,b| b <=> a}
+  return arrays
+end
