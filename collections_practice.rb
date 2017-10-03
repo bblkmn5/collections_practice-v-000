3a0a7d267462999b059d92caf21fa9f2ae075332
Ben Blackman <bblkmn5@gmail.com>
2017-10-03 03:38:35 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,19 @@
 
+def sort_array_asc(array)
+  return array.sort
+end
+
+def sort_array_desc(array)
+  arrays = array.sort {|a,b| b <=> a}
+  return arrays
+end
+
+def sort_array_char_count(array)
+  arrays = array.sort {|a, b| a.length <=> b.length}
+  return arrays
+end
+
+def swap_elements(array)
+  arrays = array.sort {|a, b| array.index[1] = a array.index[2] = b}
+  return arrays
+end
