79a07548ff8e8bc70fbf79ce9973c1aa53c6546b
Ben Blackman <bblkmn5@gmail.com>
2017-10-03 03:42:14 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,8 +14,8 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array.index[1] = a 
+  array.index[1] = a
   array.index[2] = b
-  arrays = array.sort (a <=> b)
+  arrays = array.sort {|left, right| a <=> b}
   return arrays
 end
